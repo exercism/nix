d55e874c4e8b15707e1239936fef5c602270931f
ricmaps <ricardomapurungajunior@gmail.com>
2026-07-08 20:39:56 -0300

    Move tests to nix-unit, update ci

--- docs/TESTS.md
+++ docs/TESTS.md
@@ -1,39 +1,22 @@
 # Tests
 
-We use the `lib.debug.runTests` function from the Nixpkgs standard library to run the tests.
+We use the `nix-unit` package to run the tests.
 
 ## Running the Tests
 
 Navigate to the exercise folder and run:
 
 ```shell
-nix eval --raw --impure -f {exercise}-test.nix
-```
-
-Or alternatively:
-
-```shell
-nix-instantiate --eval --strict {exercise}-test.nix
+nix-unit {exercise}-test.nix
 ```
 
 ## If You See "cannot look up nixpkgs in pure evaluation mode"
 
 The test files import `<nixpkgs/lib>`, which requires nixpkgs to be on your `NIX_PATH`.
 This is set up automatically on NixOS, but may not be on other systems.
-
-If you get this error, pass nixpkgs explicitly:
-
-```shell
-nix eval --raw --impure \
-  -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz \
-  -f {exercise}-test.nix
-```
-
-Or add nixpkgs to your channels permanently:
+If you get this error, add nixpkgs to your channels:
 
 ```shell
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
 nix-channel --update
 ```
-
-After that, the standard `nix eval --raw --impure -f {exercise}-test.nix` command will work.
--- exercises/practice/reverse-string/reverse-string-test.nix
+++ exercises/practice/reverse-string/reverse-string-test.nix
@@ -1,41 +1,34 @@
-with builtins;
 let
-  lib = import <nixpkgs/lib>;
-  exercise = import ./reverse-string.nix;
-  results = lib.debug.runTests {
-    testAnEmptyString = {
-      expr = exercise.reverse "";
-      expected = "";
-    };
-    testAWord = {
-      expr = exercise.reverse "robot";
-      expected = "tobor";
-    };
-    testACapitalizedWord = {
-      expr = exercise.reverse "Ramen";
-      expected = "nemaR";
-    };
-    testASentenceWithPunctuation = {
-      expr = exercise.reverse "I'm hungry!";
-      expected = "!yrgnuh m'I";
-    };
-    testAPalindrome = {
-      expr = exercise.reverse "racecar";
-      expected = "racecar";
-    };
-    testAnEvenSizedWord = {
-      expr = exercise.reverse "drawer";
-      expected = "reward";
-    };
-  };
+  inherit (import ./reverse-string.nix) reverse;
 in
-if results == [ ] then
-  "All tests passed!\n"
-else
-  deepSeq (map (
-    t:
-    trace ''
-      ${t.name}:
-        expected: ${t.expected}
-        result: ${t.result}'' t
-  ) results) (throw "${toString (length results)} tests failed!\n")
+{
+  "test an empty string" = {
+    expr = reverse "";
+    expected = "";
+  };
+
+  "test a word" = {
+    expr = reverse "robot";
+    expected = "tobor";
+  };
+
+  "test a capitalized word" = {
+    expr = reverse "Ramen";
+    expected = "nemaR";
+  };
+
+  "test a sentence with punctuation" = {
+    expr = reverse "I'm hungry!";
+    expected = "!yrgnuh m'I";
+  };
+
+  "test a palindrome" = {
+    expr = reverse "racecar";
+    expected = "racecar";
+  };
+
+  "test an even sized word" = {
+    expr = reverse "drawer";
+    expected = "reward";
+  };
+}
