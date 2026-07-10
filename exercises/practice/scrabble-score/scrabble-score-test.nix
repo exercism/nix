7195fdfb8e6eb5b0179e1b0e17a29bbb777c03b8
Ricardo Mapurunga Junior <ricardomapurungajunior@proton.me>
2026-07-10 01:40:28 -0300

    Add Scrabble Score (#81)

---
+++ exercises/practice/scrabble-score/scrabble-score-test.nix
@@ -0,0 +1,59 @@
+let
+  inherit (import ./.meta/example.nix) score;
+in
+{
+  "test lowercase letter" = {
+    expr = score "a";
+    expected = 1;
+  };
+
+  "test uppercase letter" = {
+    expr = score "A";
+    expected = 1;
+  };
+
+  "test valuable letter" = {
+    expr = score "f";
+    expected = 4;
+  };
+
+  "test short word" = {
+    expr = score "at";
+    expected = 2;
+  };
+
+  "test short, valuable word" = {
+    expr = score "zoo";
+    expected = 12;
+  };
+
+  "test medium word" = {
+    expr = score "street";
+    expected = 6;
+  };
+
+  "test medium, valuable word" = {
+    expr = score "quirky";
+    expected = 22;
+  };
+
+  "test long, mixed-case word" = {
+    expr = score "OxyphenButazone";
+    expected = 41;
+  };
+
+  "test english-like word" = {
+    expr = score "pinata";
+    expected = 8;
+  };
+
+  "test empty input" = {
+    expr = score "";
+    expected = 0;
+  };
+
+  "test entire alphabet available" = {
+    expr = score "abcdefghijklmnopqrstuvwxyz";
+    expected = 87;
+  };
+}
